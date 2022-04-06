92ac10dc872e36c5d5a2b1d939974d24a662a7f8
duducorrea <e.correa@aluno.ifsp.edu.br>
2022-04-06 20:34:01 -0300

    arquivo structs

---
+++ structs.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+struct medico{
+    char crm[20];
+    char nome[40];
+    char nascimento[15];
+    char sexo[10];
+    char especialidade[100];
+    char universidade[50];
+    char email[2][50];
+    char telefone[2][20];
+};
+
+struct paciente{
+    char cpf[18];
+    char nome[40];
+    char nascimento[15];
+    char sexo[10];
+    char plano[40];
+    char email[2][50];
+    char telefone[2][20];
+};
+
+struct consulta{
+    char crm[20];
+    char cpf[18];
+    char data[15];
+    char hora[6];
+    char diagnostico[100];
+    char medicamentos[5][50];
+};
+
+
+int main(){
+    
+}
